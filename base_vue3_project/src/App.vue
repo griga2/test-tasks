9604c95e913c2dca4cc962a6d8cc237be2f4171e
griga2 <griga2999wow@gmail.com>
2024-08-14 02:00:42 +0700

    create project

---
+++ base_vue3_project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <main>
+        main
+        <RouterView></RouterView>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
